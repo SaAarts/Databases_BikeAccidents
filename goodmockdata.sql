b07a96e54857e7c4f1c08c40745a725f9379b534
Almos Marton Bakonyi <almosbakonyi177@gmail.com>
2026-09-18 12:15:34 +0200

    Made the inserts in the first 4 tables.

---
+++ goodmockdata.sql
@@ -0,0 +1,31 @@
+INSERT INTO 'Bicycle' Values
+('B001', 'Giant', 0, 0, 5),
+('B005', 'Giant', 0, 0, 3),
+('B002', 'Trek', 1, 0, 2),
+('B003', 'Specialized', 0, 1, 4),
+('B004', 'Cannondale', 0, 0, 6)
+;
+
+INSERT INTO 'Cyclist' Values
+(361738163, 25, 1),
+(361738164, 30, 0),
+(361738165, 22, 1),
+(361738166, 28, 0),
+(361738167, 35, 1)
+;
+
+INSERT INTO 'DownfallType' Values
+(1, 'Rain'),
+(2, 'Snow'),
+(3, 'Hail'),
+(4, 'Sleet'),
+(5, 'Fog'),
+(6, 'Drizzle');
+
+INSERT INTO 'ReasonType' Values
+(1, 'Slippery road'),
+(2, 'Poor visibility'),
+(3, 'Mechanical failure'),
+(4, 'Human error'),
+(5, 'Animal crossing')
+;
